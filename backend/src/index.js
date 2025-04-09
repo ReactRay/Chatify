e4fea9a4d9a8c96f95de9be13e5fdd745d5d5865
ReactRay <raydiaz1899@gmail.com>
2025-04-09 10:21:01 +0300

    add express server

---
+++ backend/src/index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.use('/api/users', userRoutes)
+// app.use('/api/auth', authRoutes)
+// app.use('/api/admin', adminRoutes)
+// app.use('/api/songs', songRoutes)
+// app.use('/api/albums', albumRoutes)
+// app.use('api/stats', statsRoutes)
+
+app.listen(PORT, () => {
+  console.log(`server is running on http://localhost:${PORT}`)
+})
