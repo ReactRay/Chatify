7dd4bb19397b9e50679638f9e023825bc5fe3ac2
ReactRay <raydiaz1899@gmail.com>
2025-04-09 10:49:39 +0300

    add express routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import userRoutes from './routes/user.route.js'
+import userRoutes from './routes/user.routes.js'
+import adminRoutes from './routes/admin.routes.js'
+import authRoutes from './routes/auth.routes.js'
+import songRoutes from './routes/song.routes.js'
+import albumRoutes from './routes/album.routes.js'
+import statsRoutes from './routes/stat.routes.js'
 
 dotenv.config()
 
@@ -9,11 +14,11 @@ const app = express()
 const PORT = process.env.PORT
 
 app.use('/api/users', userRoutes)
-// app.use('/api/auth', authRoutes)
-// app.use('/api/admin', adminRoutes)
-// app.use('/api/songs', songRoutes)
-// app.use('/api/albums', albumRoutes)
-// app.use('api/stats', statsRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/admin', adminRoutes)
+app.use('/api/songs', songRoutes)
+app.use('/api/albums', albumRoutes)
+app.use('/api/stats', statsRoutes)
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`)
--- backend/src/routes/stat.routes.js
+++ backend/src/routes/stat.routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const router = Router()
 
 router.get('/', (req, res) => {
-  res.send('admin route')
+  res.send('stats route')
 })
 
 export default router
