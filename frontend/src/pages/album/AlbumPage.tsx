07759d2483f5e3fd851593b47383a150fed2e7c2
ReactRay <raydiaz1899@gmail.com>
2025-04-13 14:04:56 +0300

    finish album page

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -1,8 +1,22 @@
 
+import { Button } from "@/components/ui/button";
 import { useMusicStore } from "@/stores/useMusicStore";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Clock, Play } from "lucide-react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+
+
+function formatDuration(seconds: number) {
+
+    const minutes = Math.floor(seconds / 60)
+    const remainingSeconds = seconds % 60
+
+    return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`
+
+
+
+}
 
 export function AlbumPage() {
 
@@ -47,6 +61,66 @@ export function AlbumPage() {
                         </div>
                     </div>
 
+                    <div className='px-6 pb-4 flex items-center gap-6'>
+                        <Button
+                            size='icon'
+                            className='w-14 h-14 rounded-full bg-green-500 hover:bg-green-400 
+                					hover:scale-105 transition-all'
+                        >
+                            <Play className='h-7 w-7 text-black' />
+
+                        </Button>
+                    </div>
+
+                    {/* table section */}
+
+                    <div className="bg-black/20 backdrop-blur-sm">
+                        <div
+                            className='grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm 
+          						  text-zinc-400 border-b border-white/5'
+                        >
+                            <div>#</div>
+                            <div>Title</div>
+                            <div>Released Date</div>
+                            <div>
+                                <Clock className='h-4 w-4' />
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                    {/* songs list */}
+                    <div className="px-6">
+                        <div className="space-y-2 py-4">
+                            {currentAlbum?.songs.map((song, index) => {
+                                return (
+                                    <div
+                                        key={song._id}
+                                        className="grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm text-zinc-400 hover:bg-white/5 rounded-md group cursor-pointer"
+                                    >
+                                        <div className="flex items-center justify-center">
+                                            <span className="group-hover:hidden">{index + 1}</span>
+                                            <Play className="h-4 w-4 hidden group-hover:block" />
+                                        </div>
+
+                                        <div className="flex items-center gap-3">
+                                            <img src={song.imageUrl} alt={song.title} className="size-10" />
+                                            <div>
+                                                <div className="font-medium text-white">{song.title}</div>
+                                                <div>{song.artist}</div>
+                                            </div>
+                                        </div>
+
+                                        <div className="flex items-center">{song.createdAt.split('T')[0]}</div>
+                                        <div className="flex items-center">{formatDuration(song.duration)}</div>
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+
+                    </div>
 
                 </div>
             </div>
