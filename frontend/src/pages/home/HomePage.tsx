55a2db204d13f104857171521a2bc37fc2f4eb7d
ReactRay <raydiaz1899@gmail.com>
2025-04-13 15:54:55 +0300

    add homepage grids

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -3,6 +3,9 @@
 import { Topbar } from "@/components/ui/Topbar";
 import { useMusicStore } from "@/stores/useMusicStore";
 import { useEffect } from "react";
+import { FeaturedSection } from "./components/FeaturedSection";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import SectionGrid from "./components/SectionGrid";
 
 export function HomePage() {
 
@@ -18,7 +21,18 @@ export function HomePage() {
   console.log({ madeForYouSongs, trendingSongs, featuredSongs })
 
   return (
-    <div className="rounded-md overflow-hidden">
+    <main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
       <Topbar />
-    </div>)
+      <ScrollArea className='h-[calc(100vh-180px)]'>
+        <div className='p-4 sm:p-6'>
+          <h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+          <FeaturedSection />
+
+          <div className='space-y-8'>
+            <SectionGrid title='Made For You' songs={madeForYouSongs} isLoading={isLoading} />
+            <SectionGrid title='Trending' songs={trendingSongs} isLoading={isLoading} />
+          </div>
+        </div>
+      </ScrollArea>
+    </main>)
 }
