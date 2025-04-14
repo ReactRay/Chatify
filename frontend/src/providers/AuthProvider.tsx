28743fa0836a11f86e10dd5be6b1902a35aa3817
ReactRay <raydiaz1899@gmail.com>
2025-04-14 11:48:32 +0300

    add sockets to the frontend, no ui

--- frontend/src/providers/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider.tsx
@@ -1,51 +1,44 @@
-import { axiosInstance } from '@/lib/axios'
-import { useAuth } from '@clerk/clerk-react'
-import { Children, useEffect, useState } from 'react'
-import { Loader } from 'lucide-react'
-import { useAuthStore } from '@/stores/useAuthStore'
+import { axiosInstance } from "@/lib/axios";
+import { useAuthStore } from "@/stores/useAuthStore";
+import { useChatStore } from "@/stores/useChatStore";
+import { useAuth } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import { useEffect, useState } from "react";
 
+const updateApiToken = (token: string | null) => {
+  if (token) axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  else delete axiosInstance.defaults.headers.common["Authorization"];
+};
 
-
-
-export function AuthProvider({ children }: { children: React.ReactNode }) {
-  const { getToken } = useAuth()
-
-  const [loading, setLoading] = useState(true)
-
-  const { checkAdminStatus } = useAuthStore()
-
-  //helper
-  function updateApiToken(token: string | null) {
-    if (token) {
-      axiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`
-    }
-    else {
-      delete axiosInstance.defaults.headers.common['Authorization']
-    }
-  }
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { getToken, userId } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const { checkAdminStatus } = useAuthStore();
+  const { initSocket, disconnectSocket } = useChatStore();
 
   useEffect(() => {
     const initAuth = async () => {
       try {
-        const token = await getToken()
-        updateApiToken(token)
-
+        const token = await getToken();
+        updateApiToken(token);
         if (token) {
-          await checkAdminStatus()
-
+          await checkAdminStatus();
+          // init socket
+          if (userId) initSocket(userId);
         }
-
-
-      } catch (error) {
-        updateApiToken(null)
-        console.log('Error in auth provider', error)
+      } catch (error: any) {
+        updateApiToken(null);
+        console.log("Error in auth provider", error);
       } finally {
-        setLoading(false)
+        setLoading(false);
       }
-    }
+    };
 
-    initAuth()
-  }, [getToken])
+    initAuth();
+
+    // clean up
+    return () => disconnectSocket();
+  }, [getToken, userId, checkAdminStatus, initSocket, disconnectSocket]);
 
   if (loading)
     return (
@@ -54,7 +47,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
       </div>
     );
 
-
-
-  return <div>{children}</div>
-}
+  return <>{children}</>;
+};
+export default AuthProvider;
