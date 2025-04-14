0f5b6018caff47fd4ea84541ebb8d4bcf12d7f25
ReactRay <raydiaz1899@gmail.com>
2025-04-14 09:07:44 +0300

    make layout more responsive

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -5,38 +5,51 @@ import { LeftSidebar } from "./components/LeftSidebar";
 import { FriendsActivity } from "./components/FriendsActivity";
 import AudioPlayer from "./components/AudioPlayer";
 import { PlaybackControls } from "./components/PlaybackControls";
+import { useEffect, useState } from "react";
 
 export function MainLayout() {
 
-  const isMobile = false;
+  const [isMobile, setIsMobile] = useState(false);
 
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
 
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   return (
-    <div className="h-screen bg-black text-white flex flex-col">
+    <div className='h-screen bg-black text-white flex flex-col'>
       <ResizablePanelGroup direction='horizontal' className='flex-1 flex h-full overflow-hidden p-2'>
-        {/* Left side*/}
         <AudioPlayer />
+        {/* left sidebar */}
         <ResizablePanel defaultSize={20} minSize={isMobile ? 0 : 10} maxSize={30}>
           <LeftSidebar />
         </ResizablePanel>
 
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-        {/* main */}
+        <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
+
+        {/* Main content */}
         <ResizablePanel defaultSize={isMobile ? 80 : 60}>
           <Outlet />
         </ResizablePanel>
-        <ResizableHandle className="w-2 bg-black rounded-lg transition-colors" />
-        {/* right side */}
-        <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
-          <FriendsActivity />
 
-        </ResizablePanel>
+        {!isMobile && (
+          <>
+            <ResizableHandle className='w-2 bg-black rounded-lg transition-colors' />
 
+            {/* right sidebar */}
+            <ResizablePanel defaultSize={20} minSize={0} maxSize={25} collapsedSize={0}>
+              <FriendsActivity />
+            </ResizablePanel>
+          </>
+        )}
       </ResizablePanelGroup>
 
       <PlaybackControls />
-
     </div>
-  )
-}
+  );
+};
