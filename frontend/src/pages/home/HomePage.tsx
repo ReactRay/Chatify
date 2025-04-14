c6a888b2621a1572b42709ccfeb57e5df1c51d7b
ReactRay <raydiaz1899@gmail.com>
2025-04-14 09:04:08 +0300

    make featured and section grid work as queue

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -6,19 +6,34 @@ import { useEffect } from "react";
 import { FeaturedSection } from "./components/FeaturedSection";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import SectionGrid from "./components/SectionGrid";
+import { usePlayerStore } from "@/stores/usePlayerStore";
 
 export function HomePage() {
 
-  const { fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs, isLoading, featuredSongs, madeForYouSongs, trendingSongs } = useMusicStore()
+  const {
+    fetchFeaturedSongs,
+    fetchMadeForYouSongs,
+    fetchTrendingSongs,
+    isLoading,
+    madeForYouSongs,
+    featuredSongs,
+    trendingSongs,
+  } = useMusicStore();
 
+  const { initializeQueue } = usePlayerStore();
 
   useEffect(() => {
-    fetchFeaturedSongs()
-    fetchMadeForYouSongs()
-    fetchTrendingSongs()
-  }, [fetchFeaturedSongs, fetchTrendingSongs, fetchMadeForYouSongs])
+    fetchFeaturedSongs();
+    fetchMadeForYouSongs();
+    fetchTrendingSongs();
+  }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs]);
 
-  console.log({ madeForYouSongs, trendingSongs, featuredSongs })
+  useEffect(() => {
+    if (madeForYouSongs.length > 0 && featuredSongs.length > 0 && trendingSongs.length > 0) {
+      const allSongs = [...featuredSongs, ...madeForYouSongs, ...trendingSongs];
+      initializeQueue(allSongs);
+    }
+  }, [initializeQueue, madeForYouSongs, trendingSongs, featuredSongs]);
 
   return (
     <main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
@@ -34,5 +49,6 @@ export function HomePage() {
           </div>
         </div>
       </ScrollArea>
-    </main>)
-}
+    </main>
+  );
+};
