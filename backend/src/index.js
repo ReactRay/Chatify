bce94cd3a2c41d97faa9b40708bc904e13d6bf04
ReactRay <raydiaz1899@gmail.com>
2025-04-15 12:47:51 +0300

    ready for deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -71,11 +71,12 @@ app.use('/api/albums', albumRoutes)
 app.use('/api/stats', statsRoutes)
 
 
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "../frontend/dist")));
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
-	});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get(/^\/(?!api).*/, (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'))
+  })
 }
 
 
